7de10b463b7198add381d15f6d2a4b1f57e232c2
LCatSew <119518206+LCatSew@users.noreply.github.com>
2023-08-30 23:21:36 -0500

    added HTML and API routes

--- develop/public/assets/server.js
+++ develop/public/assets/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
-
 const path = require('path');
-
+const dbData = require('/develop/db/db.json');
 const app = express();
 
 const PORT = 3001;
 
 //HTML ROUTES//
-
 app.use(express.static('public'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'develop/public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'develop/public/notes.html')));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/develop/public/index.html')));
+//API ROUTES//
+app.get('/api/notes', (req, res) => res.json(dbData));
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/develop/public/notes.html')));
+app.listen(PORT, () => 
+    console.log(`Example app listening at http://localhost:${PORT}`)
+);
